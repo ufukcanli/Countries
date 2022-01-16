ebdd20bca1544b0e0557432d478eb67232e49930
ufukcanli <iletisim@ufukcanli.com>
2022-01-16 16:59:20 +0300

    Add initial setup

---
+++ Countries/Configuration/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Countries
+//
+//  Created by Ufuk Canlı on 16.01.2022.
+//
+
+import UIKit
+import SDWebImageSVGCoder
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let SVGCoder = SDImageSVGCoder.shared
+        SDImageCodersManager.shared.addCoder(SVGCoder)
+        return true
+    }
+
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+}
